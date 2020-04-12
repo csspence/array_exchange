1bcb0cf519638126fbb81130a79e130adadfe26b
Crew Spence <csspence123@gmail.com>
2020-04-12 05:02:59 -0500

    Created function stub for Code Wars toy problem.

---
+++ array_exchange.js
@@ -0,0 +1,15 @@
+/*
+// before
+const myArray = ['a', 'b', 'c'];
+const otherArray = [1, 2, 3];
+
+exchangeWith(myArray, otherArray);
+
+// after
+myArray => [3, 2, 1]
+otherArray => ['c', 'b', 'a']
+*/
+
+function exchangeWith(a, b) {
+  // your code here
+}
