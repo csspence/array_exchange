d7aea9979191e099e7856f698d57f9cb032f3fcb
Crew Spence <csspence123@gmail.com>
2020-04-12 05:19:04 -0500

    Initial attempt failed.  Must be in place.

--- array_exchange.js
+++ array_exchange.js
@@ -10,6 +10,15 @@ myArray => [3, 2, 1]
 otherArray => ['c', 'b', 'a']
 */
 
-function exchangeWith(a, b) {
-  // your code here
+const exchangeWith = (a, b) => {
+  let newA = [];
+  let newB = [];
+  for(let i = a.length - 1; i > -1; i--) {
+    newB.push(a[i]);
+  }
+  for(let h = b.length - 1; h > -1; h--) {
+    newA.push(b[h]);
+  }
+  a = newA;
+  b = newB;
 }
